8cbb7deadcb2f784e4ee7219cf2968dcbe29d5f9
karol_fortnite <karol.koszewski@cyfroweszkoly.pl>
2025-04-02 15:07:16 +0200

    utworzenie selecta

--- src/Form.tsx
+++ src/Form.tsx
@@ -14,6 +14,10 @@ interface FormData{
 const Form = (props) => {
     const [formData] = useState<FormData>({name: ""})
 
+    function handleChange() {
+
+    }
+
     return (
         <>
             <input
@@ -21,8 +25,14 @@ const Form = (props) => {
                 name="name"
                 value={formData.name}
                 onChange={handleChange}
-
             />
+            <select name="powodWymowki" value={formData.powod}
+                    onChange={handleChange}>
+                <option value="">-- Select --</option>
+                <option value="spoznienie">spóźnienie</option>
+                <option value="niedostarczeniePracyDomowej">niedostarczenie pracy domowej</option>
+                <option value="brakOdpowiedziNaWiaomosc">brak odpowiedzi na wiadomość</option>
+            </select>
         </>
     )
 }
